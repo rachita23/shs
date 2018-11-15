9a177fff756d42daa31214e382048674ad75bedf
Anuparna Deb <44692463+anuparna18001@users.noreply.github.com>
2018-11-15 15:14:14 +0530

    Update Doctor_Profile.java

--- src/project/Doctor_Profile.java
+++ src/project/Doctor_Profile.java
@@ -17,7 +17,7 @@ public class Doctor_Profile extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void Doctor_Profile() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
