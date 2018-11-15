21b30331779d0870495bffe0dda9b7ebfce65090
Anuparna Deb <44692463+anuparna18001@users.noreply.github.com>
2018-11-15 15:09:33 +0530

    Update BookAppt.java

--- src/project/BookAppt.java
+++ src/project/BookAppt.java
@@ -18,7 +18,7 @@ public class BookAppt extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void BookAppt {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
