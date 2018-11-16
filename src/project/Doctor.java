f3a0f7de965131510fb2c47ad88b9055d79f8103
Shubham-Singhal <44692766+Shubham-Singhal@users.noreply.github.com>
2018-11-16 11:05:35 +0530

    Add files via upload

---
+++ src/project/Doctor.java
@@ -0,0 +1,39 @@
+
+public class Doctor extends Person {
+
+	private int id;
+	private String appt;
+	private int deptid;
+	private String position;
+	private int roomno;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAppt() {
+		return appt;
+	}
+	public void setAppt(String appt) {
+		this.appt = appt;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public int getDeptid() {
+		return deptid;
+	}
+	public void setDeptid(int deptid) {
+		this.deptid = deptid;
+	}
+	public int getRoomno() {
+		return roomno;
+	}
+	public void setRoomno(int roomno) {
+		this.roomno = roomno;
+	}
+}
---
+++ src/project/GetDocID.java
@@ -0,0 +1,50 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class GetDocID extends JFrame {
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GetDocID frame = new GetDocID();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GetDocID() {
+		setTitle("Doctor ID");
+		setBounds(100, 100, 202, 147);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		JLabel lb = new JLabel("Enter ID: ");
+		lb.setBounds(10, 11, 54, 14);
+		getContentPane().add(lb);
+		
+		textField = new JTextField();
+		textField.setBounds(74, 8, 107, 20);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btn = new JButton("Enter");
+		btn.setBounds(48, 62, 89, 23);
+		getContentPane().add(btn);
+
+	}
+}
