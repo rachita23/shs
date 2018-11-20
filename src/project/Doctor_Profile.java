4317af9ef97204ff4a82c7171e4032e0aca53b01
rachita <rachita18044@iiitd.ac.in>
2018-11-20 09:04:05 +0530

    updates

--- src/project/Doctor_Profile.java
+++ src/project/Doctor_Profile.java
@@ -8,21 +8,25 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.awt.event.ActionEvent;
 
 public class Doctor_Profile extends JFrame {
 
 	private JPanel contentPane;
-
+	public int id;
 	/**
 	 * Launch the application.
 	 */
-	public static void Doctor_Profile() {
+	public static void Doctor_Profile(int id) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					Doctor_Profile frame = new Doctor_Profile();
 					frame.setVisible(true);
+					frame.id = id;
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -38,9 +42,33 @@ public class Doctor_Profile extends JFrame {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 307, 458);
 		contentPane = new JPanel();
+		String name = "",gender=null,eid=null,address=null,at=null,pos=null;
+		int age=0,rn=0,dept=0;//,id=0;
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		String q = "select * from doctor where id ='"+id+"'";
+		//JOptionPane.showMessageDialog(this, q);
+		try{
+			Connection con = Connection_DB.main();
+			Statement s = con.createStatement();
+			ResultSet rs = s.executeQuery(q);
+			while(rs.next()) {
+				name = rs.getString("name");
+				gender = rs.getString("gender");
+				eid = rs.getString("email");
+				address = rs.getString("address");
+				at = rs.getString("appt_time");
+				pos = rs.getString("position");
+				age = rs.getInt("age");
+				rn = rs.getInt("roomno");
+				id = rs.getInt("id");
+				dept = rs.getInt("dept");
+			}
+		}
+			catch (Exception z) {
+				z.printStackTrace();
+			}
 		
 		JLabel lblNewLabel = new JLabel("Name:");
 		lblNewLabel.setBounds(10, 55, 46, 14);
@@ -65,38 +93,46 @@ public class Doctor_Profile extends JFrame {
 		JLabel lb1 = new JLabel("Rachita Nayyar");
 		lb1.setBounds(81, 55, 181, 14);
 		contentPane.add(lb1);
+		lb1.setText(name);
 		
 		JLabel lb4 = new JLabel("30");
 		lb4.setBounds(81, 105, 181, 14);
 		contentPane.add(lb4);
+		lb4.setText(Integer.toString(age));
 		
 		JLabel lb5 = new JLabel("rachitanayyar@");
 		lb5.setBounds(81, 134, 181, 14);
 		contentPane.add(lb5);
+		lb5.setText(eid);
 		
 		JLabel lb6 = new JLabel("Inderpuri");
 		lb6.setBounds(81, 159, 181, 25);
 		contentPane.add(lb6);
+		lb6.setText(address);
 		
 		JLabel lb3 = new JLabel("Female");
 		lb3.setBounds(81, 80, 181, 14);
 		contentPane.add(lb3);
+		lb3.setText(gender);
 		
 		JLabel lblPosition = new JLabel("Position:");
 		lblPosition.setBounds(10, 205, 46, 14);
 		contentPane.add(lblPosition);
 		
-		JLabel lblNeurologist = new JLabel("Neurologist");
-		lblNeurologist.setBounds(81, 205, 156, 14);
-		contentPane.add(lblNeurologist);
+		
+		JLabel lblP = new JLabel("Neurologist");
+		lblP.setBounds(81, 205, 156, 14);
+		contentPane.add(lblP);
+		lblP.setText(pos);
 		
 		JLabel lblAppttime = new JLabel("ApptTime:");
 		lblAppttime.setBounds(10, 241, 59, 14);
 		contentPane.add(lblAppttime);
 		
-		JLabel lblMonday = new JLabel("Monday: 5:30-6:30");
-		lblMonday.setBounds(81, 241, 126, 14);
-		contentPane.add(lblMonday);
+		JLabel lblapt = new JLabel("Monday: 5:30-6:30");
+		lblapt.setBounds(81, 241, 126, 14);
+		contentPane.add(lblapt);
+		lblapt.setText(at);
 		
 		JLabel lblDeptno = new JLabel("DeptNo:");
 		lblDeptno.setBounds(10, 279, 46, 14);
@@ -105,6 +141,7 @@ public class Doctor_Profile extends JFrame {
 		JLabel label_1 = new JLabel("1");
 		label_1.setBounds(81, 279, 46, 14);
 		contentPane.add(label_1);
+		label_1.setText(Integer.toString(dept));
 		
 		JLabel lblRoomno = new JLabel("RoomNo:");
 		lblRoomno.setBounds(10, 313, 46, 14);
@@ -113,6 +150,7 @@ public class Doctor_Profile extends JFrame {
 		JLabel label_2 = new JLabel("4");
 		label_2.setBounds(81, 313, 46, 14);
 		contentPane.add(label_2);
+		label_2.setText(Integer.toString(rn));
 		
 		JButton btnNewButton = new JButton("Book Appointment");
 		btnNewButton.addActionListener(new ActionListener() {
